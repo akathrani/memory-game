819f0a023519d7e73eadb29dbc3f66fb3cc12ec1
akathrani <43335882+akathrani@users.noreply.github.com>
2019-01-27 00:28:32 +0530

    Mind Games - Diamond Sweeper Version 1 (Random Diamond element placed on board)

--- src/index.js
+++ src/index.js
@@ -9,17 +9,39 @@ ReactDOM.render(element, document.getElementById('root'));
 
 /*setInterval(tick, 1000);*/
 
+/*var diamondArray = Array.from({length: 8}, () => Math.floor(Math.random() * 64));*/
+
+function shuffle(arra1) {
+    var ctr = arra1.length, temp, index;
+
+// While there are elements in the array
+    while (ctr > 0) {
+// Pick a random index
+        index = Math.floor(Math.random() * ctr);
+// Decrease ctr by 1
+        ctr--;
+// And swap the last element with it
+        temp = arra1[ctr];
+        arra1[ctr] = arra1[index];
+        arra1[index] = temp;
+    }
+    return arra1;
+}
+
+var elementsArray = Array.from({length: 64}, (v, i) => i);
+var diamondArray = shuffle(elementsArray).slice(0,8);
+
 var listItems = (
-	Array.from(Array(8)).map((n, number) => 
+	elementsArray.map((value, index) => 
 	<li
-		className='card'
-		type="diamond"
-	  >{number}</li>
+		key={index.toString()}
+	    type={(diamondArray.indexOf(index) > -1) ? 'diamond' : ''}
+	  ></li>
 	)
 );
 
 /* gccm - grid--cards-container--multirow  */
 ReactDOM.render(
-  <ul class='gccm' aria-labelledby='cd'>{listItems}</ul>,
+  <ul className='gccm' aria-labelledby='cd'>{listItems}</ul>,
   document.getElementById('deck')
 );
